685fa3ea1b7ac0de7ed8be5e0ecee47ab24e2451
Joël ZHU <joelzhu@outlook.fr>
2021-05-14 20:53:57 +0200

    Add neutral

--- src/components/MainWindow.vue
+++ src/components/MainWindow.vue
@@ -4,6 +4,7 @@
     <button @click="newPreys()">Add</button>
     <button @click="newPredator()">Add Predator</button>
     <button @click="clear()">Clear</button>
+    <div> Preys: {{this.preyNo}} | Predators: {{this.predatorNo}} | Neutral: {{this.neutralNo}}</div>
     <div id="canvas"></div>
   </div>
 </template>
@@ -14,12 +15,16 @@ import FlockApplication from '../models/FlockApplication'
 
 export default class MainWindow extends Vue {
   start = false
+  preyNo = 0
+  predatorNo = 0
+  neutralNo = 0
   application: FlockApplication|undefined
 
   mounted () : void {
     this.application = new FlockApplication(window.innerWidth * 0.9, window.innerHeight * 0.9)
     const canvas = document.getElementById('canvas') as HTMLDivElement
     canvas.appendChild(this.application.app_.view)
+    this.boidsNo()
     this.animate(this.application)
   }
 
@@ -30,18 +35,29 @@ export default class MainWindow extends Vue {
   clear () : void {
     if (this.application !== undefined) {
       this.application.clear()
+      this.boidsNo()
     }
   }
 
   newPreys () : void {
     if (this.application !== undefined) {
       this.application.createPreys(50, 2, 0x0000FF)
+      this.boidsNo()
     }
   }
 
   newPredator () : void {
     if (this.application !== undefined) {
       this.application.createPredators(1, 1, 0xFF0000)
+      this.boidsNo()
+    }
+  }
+
+  boidsNo () : void {
+    if (this.application !== undefined) {
+      this.preyNo = this.application.preys_.boids_.length
+      this.predatorNo = this.application.predators_.boids_.length
+      this.neutralNo = this.application.neutral_.boids_.length
     }
   }
 
--- src/models/FlockApplication.ts
+++ src/models/FlockApplication.ts
@@ -11,6 +11,7 @@ export default class FlockApplication {
 
   public predators_: IFLock
   public preys_: IFLock
+  public neutral_: IFLock
 
   constructor (width: number, height: number) {
     this.app_ = new PIXI.Application({
@@ -23,12 +24,14 @@ export default class FlockApplication {
 
     this.predators_ = new PredatorFlock(width, height)
     this.preys_ = new PreyFlock(width, height)
+    this.neutral_ = new PreyFlock(width, height)
 
     this.predators_.others_.push(this.preys_)
     this.preys_.others_.push(this.predators_)
 
     this.createPredators(3, 2, 0xFF0000)
-    this.createPreys(100, 3, 0x0000FF)
+    this.createPreys(200, 3, 0x0000FF)
+    this.createNeutral(50, 3, 0x00FF00)
   }
 
   createPredators (amount = 1, MAX_VEL = 3, color = 0xFFFFFF) : void {
@@ -47,18 +50,32 @@ export default class FlockApplication {
     }
   }
 
+  createNeutral (amount = 1, MAX_VEL = 3, color = 0xFFFFFF) : void {
+    for (let i = 0; i < amount; ++i) {
+      const b = this.neutral_.createRandomBoid(MAX_VEL, color)
+      this.neutral_.boids_.push(b)
+      this.app_.stage.addChild(b.graphics_)
+    }
+  }
+
   randomNum (min = 0, max = 1) : number {
     return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+  }
+
+  amount () : number {
+    return this.app_.stage.children.length
   }
 
   clear () : void {
     this.predators_.boids_ = []
     this.preys_.boids_ = []
+    this.neutral_.boids_ = []
     this.app_.stage.removeChildren()
   }
 
   move () : void {
     this.preys_.move()
     this.predators_.move()
+    this.neutral_.move()
   }
 }
